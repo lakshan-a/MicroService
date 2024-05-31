05f917c51ab531c56a5a6cd9fa4f7c4a4f38a899
lakshan <lakshanrashmika16302@gmail.com>
2024-05-31 12:42:58 +0530

    updated

---
+++ service-regisry/src/main/java/com/example/service_regisry/ServiceRegisryApplication.java
@@ -0,0 +1,13 @@
+package com.example.service_regisry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceRegisryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegisryApplication.class, args);
+	}
+
+}
